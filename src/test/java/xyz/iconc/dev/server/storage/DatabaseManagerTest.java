43b4ef0a4abd307d9be3ed0354da36bb908794ec
IConservativeCode <gkrier04@icloud.com>
2021-11-23 09:29:04 -0700

    Creates DatabaseManager Testing Class

---
+++ src/test/java/xyz/iconc/dev/server/storage/DatabaseManagerTest.java
@@ -0,0 +1,26 @@
+package xyz.iconc.dev.server.storage;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseManagerTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void insert_createAccount() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void insert_createChannel() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isReady() {
+    }
+}
