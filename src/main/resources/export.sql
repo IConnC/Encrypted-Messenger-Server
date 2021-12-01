1b300dbf3f199faa3698298c78a173bfdfe863ba
IConservativeCode <gkrier04@icloud.com>
2021-12-01 14:45:47 -0700

    Edited SQL Database Structure File

--- src/main/resources/export.sql
+++ src/main/resources/export.sql
@@ -1,32 +1,29 @@
-SET @db = ?;
 
-CREATE DATABASE @encrypticator_database_name;
+CREATE DATABASE Encrypicator_Server_Test_1;
 
-SET @q = CONCAT('CREATE TABLE ', @db, '.accounts (
+CREATE TABLE Encrypicator_Server_Test_1.accounts (
 	user_identifier bigint(19) NOT NULL,
 	username tinytext NOT NULL,
 	hashed_password text(65535) NOT NULL,
 	epoch_registered bigint(19) NOT NULL,
 	account_type tinyint(3) NOT NULL,
 	PRIMARY KEY (user_identifier)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
-
-CREATE TABLE @encrypticator_database_name.channel_members (
+CREATE TABLE Encrypicator_Server_Test_1.channel_members (
 	channel_identifier bigint(19) DEFAULT NULL,
 	user_identifier bigint(19) DEFAULT NULL,
 	join_date bigint(19) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE @encrypticator_database_name.channels (
+CREATE TABLE Encrypicator_Server_Test_1.channels (
 	channel_identifier bigint(19) NOT NULL,
 	channel_name text(65535) NOT NULL,
 	creation_date bigint(19) NOT NULL,
 	PRIMARY KEY (channel_identifier)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE @encrypticator_database_name.messages (
+CREATE TABLE Encrypicator_Server_Test_1.messages (
 	message_identifier bigint(19) NOT NULL,
 	channel_sent_id bigint(19) NOT NULL,
 	author_identifier bigint(19) NOT NULL,
@@ -35,16 +32,11 @@ CREATE TABLE @encrypticator_database_name.messages (
 	PRIMARY KEY (message_identifier)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-PREPARE stmt FROM @q;
-EXECUTE stmt;
-DEALLOCATE PREPARE stmt;
-
-
-ALTER TABLE @encrypticator_database_name.channel_members
+ALTER TABLE Encrypicator_Server_Test_1.channel_members
 	ADD FOREIGN KEY (channel_identifier) 
 	REFERENCES Encrypicator_Server_Test_1.channels (channel_identifier) ON DELETE CASCADE ON UPDATE CASCADE;
 
-ALTER TABLE @encrypticator_database_name.channel_members
+ALTER TABLE Encrypicator_Server_Test_1.channel_members
 	ADD FOREIGN KEY (user_identifier) 
 	REFERENCES Encrypicator_Server_Test_1.accounts (user_identifier) ON DELETE CASCADE ON UPDATE CASCADE;
 
